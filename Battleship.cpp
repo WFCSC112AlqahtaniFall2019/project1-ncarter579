921e79aa7854ee1b3aa005d9fe975bbe23adccab
Nathan Carter <cartnl18@wfu.edu>
2019-09-05 16:19:45 -0400

    Fixing the vector to make it a matrix and setting up the random generators so they pick a spot on the board and change the value their from 0 to 1

--- Battleship.cpp
+++ Battleship.cpp
@@ -6,17 +6,20 @@
 using namespace std;
 
 int main() {
-    vector<int> board(2,2);
+    vector<int> board(3,0);
+    vector<vector<int>> matrix(3, board);
     int shipX;
     int shipY;
     int guessX;
     int guessY;
+    int i;
 
     srand(time(0));
-    shipX = rand()% 3 + 0;
-    shipY = rand()% 3 + 0;
+    for(i = 0; i < 2; i++ ) {
+        shipX = rand() % 3 + 0;
+        shipY = rand() % 3 + 0;
+        matrix.at(shipX).at(shipY) = 1;
+    }
 
-    cout << shipX;
-    cout << shipY;
     return 0;
 }
